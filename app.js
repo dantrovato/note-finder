791331a6f92ef709d328c1d88a2768f68f5ff3c7
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-01-10 22:28:45 +0000

    Add getIndexes() function to grab the correct indexes of any given key

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const major = [1, 5, 8];
-const minor = [1, 4, 8];
+const scales = {
+  major: [1, 5, 8],
+  minor: [1, 4, 8],
+  major7: [1, 5, 8, 11],
+  minor7: [1, 4, 8, 11],
+};
 
+// Remove highlited background red and makes it white
 function resetHighlight(notes) {
   notes.forEach((note) => (note.style.backgroundColor = "white"));
 }
 
-function highlightNotes(key, notes) {
+// Gets the correct indexes to highlight for any given key
+function getIndexes(key, scale, notes) {
+  // Makes an array of strings representing a note ["C", "C#"...] and then gets the index of the key passed in.
+  // This represents how many steps there are between the default C and the current note.
+  // This way the numbers in the scale passed in can be adjusted to the current key
+  const steps = notes.map((note) => note.textContent).indexOf(key);
+
+  // Takes the numbers inside the scale, then reduce that number by one to adjust to a 0 index array.
+  // Then it adds the number of steps to give the correct indexes
+  const indexes = scales[scale].map((idx) => idx - 1 + steps);
+  return indexes;
+}
+
+function highlightNotes(key, scale, notes) {
+  // Remove highlight from all notes
   resetHighlight(notes);
+
+  const indexes = getIndexes(key, scale, notes);
+
+  console.log(indexes);
+
   notes.forEach((note) => {
-    if (note.textContent === key.toUpperCase()) {
+    if (note.textContent === key) {
       note.style.backgroundColor = "red";
     }
   });
@@ -19,11 +43,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const submit = document.querySelector('input[type="submit"]');
 
   // Capture the notes on the DOM
-  const notes = document.querySelectorAll(".note");
+  const notes = [...document.querySelectorAll(".note")];
 
   // Handle submit
   submit.addEventListener("click", (event) => {
-    const key = document.querySelector("#enterKey").value;
-    highlightNotes(key, notes);
+    const key = document.querySelector("#enterKey").value.toUpperCase();
+    const scale = document.querySelector("#enterScale").value;
+    highlightNotes(key, scale, notes);
   });
 });
